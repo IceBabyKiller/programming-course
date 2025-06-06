277d0eb9959f4ce83e4cd82a7ee2306bd0268f3d
Stefan <manguygo@gmail.com>
2025-06-06 19:23:36 +0300

    Week 1 added

---
+++ Week1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hello " + ", " +  userName + '!');
+            Console.WriteLine($"Hello, {userName} !");
+        }
+    }
+}
