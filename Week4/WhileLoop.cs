3766b0b806e0f8b41b6d745078a457d5f91f2e46
IceBabyKiller <artsemi.kapaniou@tktk.ee>
2025-06-06 20:32:36 +0300

    Week2 and 3

---
+++ Week4/WhileLoop.cs
@@ -0,0 +1,23 @@
+Random rnd = new Random();
+int cpuRandom;
+
+bool loopAdctive = true;
+
+while (loopAdctive)
+{
+    cpuRandom = rnd.Next(1, 4);
+    Console.WriteLine("Make a guess. Enter a number 1-3");
+
+    int userNumber = Int32.Parse(Console.ReadLine());
+
+    if (userNumber == cpuRandom)
+    {
+        Console.WriteLine("Congratulations!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Try again");
+    }   
+}
+Console.WriteLine("Have a nice day!");
